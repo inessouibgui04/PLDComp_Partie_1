e8248027662b070b7ced99837db43f80d4ee2260
inessouibgui <inessouibgui2004@yahoo.com>
2026-03-02 09:43:06 +0100

    Retirer compiler/config.mk du suivi pour qu'il soit ignoré

--- compiler/config.mk
+++
@@ -1,3 +0,0 @@
-ANTLRJAR=/home/ineslinux/antlr4-install/antlr-4.13.2-complete.jar
-ANTLRINC=/home/ineslinux/antlr4-install/include/antlr4-runtime/
-ANTLRLIB=/home/ineslinux/antlr4-install/lib/libantlr4-runtime.a
